7080c08e036ef501a979ea898c9e6324410c7ea3
EthereumEliteswap <73964644+EthereumEliteswap@users.noreply.github.com>
2021-04-05 04:07:56 +0300

    Add bsc token list. Update ethereum token list

--- src/buildList.js
+++ src/buildList.js
@@ -1,6 +1,8 @@
 const { version } = require('../package.json');
 const mainnet = require('./tokens/mainnet.json');
+const bscMainnet = require('./tokens/bsc_mainnet.json');
 const ropsten = require('./tokens/ropsten.json');
+const bscTestnet = require('./tokens/bsc_testnet.json');
 
 module.exports = function buildList() {
   const parsed = version.split('.');
@@ -13,13 +15,15 @@ module.exports = function buildList() {
       'patch': +parsed[ 2 ]
     },
     'tags': {},
-    'logoURI': 'https://raw.githubusercontent.com/EthereumEliteswap/default-token-list/master/src/assets/0x380291A9A8593B39f123cF39cc1cc47463330b1F/logo.png',
+    'logoURI': 'https://raw.githubusercontent.com/EthereumEliteswap/default-token-list/master/src/assets/ethereum/0x380291A9A8593B39f123cF39cc1cc47463330b1F/ELT_logo.png',
     'keywords': [
       'eliteswap',
       'default'
     ],
     tokens: [
       ...mainnet,
+      ...bscMainnet,
+      ...bscTestnet,
       ...ropsten
     ]
       // sort them by symbol for easy readability
